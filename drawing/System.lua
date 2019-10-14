b1ad4fcfa77acc4e2bc896d97f63453dd54f2913
ndwarshuis <ndwar@yavin4.ch>
2019-10-13 21:13:00 -0400

    ENH make kernel part of the static image

--- drawing/System.lua
+++ drawing/System.lua
@@ -9,11 +9,6 @@ local __string_match = string.match
 
 local _TEXT_SPACING_ = 20
 
-local extract_date = function(cmd)
-	local yyyy, mm_dd = __string_match(Util.execute_cmd(cmd), '%[(%d-)%-(%d-%-%d-)%s')
-	return mm_dd..'-'..yyyy
-end
-
 local header = _G_Widget_.Header{
 	x = _G_INIT_DATA_.LEFT_X,
 	y = _G_INIT_DATA_.TOP_Y,
@@ -30,33 +25,40 @@ local labels = _G_Widget_.TextColumn{
 	'Last Upgrade',
 	'Last Sync'
 }
+local kernel = _G_Widget_.Text{
+   x          = _G_INIT_DATA_.LEFT_X + _G_INIT_DATA_.SECTION_WIDTH,
+   y          = header.bottom_y,
+   x_align    = 'right',
+   text       = Util.conky('$kernel'),
+   text_color = _G_Patterns_.BLUE
+}
 local info = _G_Widget_.TextColumn{
 	x 			= _G_INIT_DATA_.LEFT_X + _G_INIT_DATA_.SECTION_WIDTH,
-	y 			= header.bottom_y,
+	y 			= header.bottom_y + _TEXT_SPACING_,
 	spacing 	= _TEXT_SPACING_,
 	x_align 	= 'right',
 	text_color 	= _G_Patterns_.BLUE,
-	Util.conky('$kernel'),
+	'<row1>',
 	'<row2>',
-	'<row3>',
-	'<row4>'
+	'<row3>'
 }
 
 _TEXT_SPACING_ = nil
 
 local draw_static = function(cr)
    Text.draw(header.text, cr)
+   Text.draw(kernel, cr)
    Line.draw(header.underline, cr)
    TextColumn.draw(labels, cr)
 end
 
 local draw_dynamic = function(cr, pacman_stats)
-   TextColumn.set(info, cr, 2, Util.conky('$uptime'))
+   TextColumn.set(info, cr, 1, Util.conky('$uptime'))
 
    if pacman_stats then
       local last_update, last_sync = __string_match(pacman_stats, "^%d+%s+([^%s]+)%s+([^%s]+).*")
-      TextColumn.set(info, cr, 3, last_update)
-      TextColumn.set(info, cr, 4, last_sync)
+      TextColumn.set(info, cr, 2, last_update)
+      TextColumn.set(info, cr, 3, last_sync)
    end
 
    TextColumn.draw(info, cr)
