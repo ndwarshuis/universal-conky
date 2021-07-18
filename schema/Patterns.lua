3953b97964a94d1b065b40ee2e9959bb1c4f3ba4
ndwarshuis <ndwar@yavin4.ch>
2021-07-18 17:55:43 -0400

    ENH change color scheme back to light blue

--- schema/Patterns.lua
+++ schema/Patterns.lua
@@ -6,21 +6,21 @@ local Util 	= require 'Util'
 M.FONT = 'Neuropolitical'
 
 -- text colors
-M.HEADER_FG = Color.rgb(0xe0e0e0)
+M.HEADER_FG = Color.rgb(0xefefef)
 
-M.PRIMARY_FG = Color.rgb(0xB0A6ED)
+M.PRIMARY_FG = Color.rgb(0xbfe1ff)
 M.CRITICAL_FG = Color.rgb(0xff8282)
 
-M.INACTIVE_TEXT_FG = Color.rgb(0xb7b7b7)
-M.MID_GREY = Color.rgb(0xa6a6a6)
-M.BORDER_FG = Color.rgb(0x5a5a5a)
-M.PLOT_GRID_FG = Color.rgb(0x565656)
-M.PLOT_OUTLINE_FG = Color.rgb(0x626262)
+M.INACTIVE_TEXT_FG = Color.rgb(0xc8c8c8)
+M.MID_GREY = Color.rgb(0xd6d6d6)
+M.BORDER_FG = Color.rgb(0x888888)
+M.PLOT_GRID_FG = Color.rgb(0x666666)
+M.PLOT_OUTLINE_FG = Color.rgb(0x777777)
 
 
 -- arc bg colors
-local GREY2 = 0x939393
-local GREY5 = 0x323232
+local GREY2 = 0xbfbfbf
+local GREY5 = 0x565656
 M.INDICATOR_BG = Color.gradient_rgb{
    [0.0] = GREY5,
    [0.5] = GREY2,
@@ -28,8 +28,8 @@ M.INDICATOR_BG = Color.gradient_rgb{
 }
 
 -- arc/bar fg colors
-local PRIMARY1 = 0x9E90E6
-local PRIMARY3 = 0x432DB3
+local PRIMARY1 = 0x99CEFF
+local PRIMARY3 = 0x316BA6
 M.INDICATOR_FG_PRIMARY = Color.gradient_rgb{
    [0.0] = PRIMARY3,
    [0.5] = PRIMARY1,
@@ -45,10 +45,10 @@ M.INDICATOR_FG_CRITICAL = Color.gradient_rgb{
 }
 
 -- plot colors
-local PLOT_PRIMARY1 = 0x15007C
-local PLOT_PRIMARY2 = 0x431EFF
-local PLOT_PRIMARY3 = 0x4B31CE
-local PLOT_PRIMARY4 = 0x9F8CFF
+local PLOT_PRIMARY1 = 0x003f7c
+local PLOT_PRIMARY2 = 0x1e90ff
+local PLOT_PRIMARY3 = 0x316ece
+local PLOT_PRIMARY4 = 0x8cc7ff
 M.PLOT_FILL_BORDER_PRIMARY = Color.gradient_rgb{
    [0.0] = PLOT_PRIMARY1,
    [1.0] = PLOT_PRIMARY2
