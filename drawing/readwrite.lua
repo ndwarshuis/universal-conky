088979312d4ae69953534429dd453706b2cf0db8
ndwarshuis <ndwar@yavin4.ch>
2022-07-14 22:59:14 -0400

    REF make io module use setup framework

--- drawing/readwrite.lua
+++ drawing/readwrite.lua
@@ -1,4 +1,5 @@
 local format = require 'format'
+local pure = require 'pure'
 local common = require 'common'
 local geometry = require 'geometry'
 local sys = require 'sys'
@@ -9,15 +10,19 @@ return function(update_freq, devices, point)
    -- TODO currently this will find any block device
    local DEVICE_PATHS = sys.get_disk_paths(devices)
 
-   local init_read_bytes, init_write_bytes = sys.get_total_disk_io(DEVICE_PATHS)
+   local state = {read = 0, write = 0}
+   state.read, state.write = sys.get_total_disk_io(DEVICE_PATHS)
 
    local format_value_function = function(bps)
       local unit, value = format.convert_data_val(bps)
       return format.precision_round_to_string(value, 3)..' '..unit..'B/s'
    end
 
-   local make_plot = function(y, label, init)
-      return common.make_rate_timeseries(
+   -----------------------------------------------------------------------------
+   -- r/w plots
+
+   local mk_plot = function(label, key, y)
+      local obj = common.make_rate_timeseries(
          point.x,
          y,
          geometry.SECTION_WIDTH,
@@ -28,53 +33,39 @@ return function(update_freq, devices, point)
          label,
          2,
          update_freq,
-         init
+         state[key]
+      )
+      return common.mk_acc(
+         PLOT_HEIGHT + PLOT_SEC_BREAK,
+         function() common.update_rate_timeseries(obj, state[key]) end,
+         pure.partial(common.tagged_scaled_timeseries_draw_static, obj),
+         pure.partial(common.tagged_scaled_timeseries_draw_dynamic, obj)
       )
    end
 
-   -----------------------------------------------------------------------------
-   -- header
-
-   local header = common.make_header(
-      point.x,
-      point.y,
-      geometry.SECTION_WIDTH,
-      'INPUT / OUTPUT'
-   )
-
-   -----------------------------------------------------------------------------
-   -- reads
-
-   local reads = make_plot(header.bottom_y, 'Reads', init_read_bytes)
-
-   -----------------------------------------------------------------------------
-   -- writes
-
-   local writes = make_plot(
-      header.bottom_y + PLOT_HEIGHT + PLOT_SEC_BREAK * 2,
-      'Writes',
-      init_write_bytes
-   )
+   local mk_reads = pure.partial(mk_plot, 'Reads', 'read')
+   local mk_writes = pure.partial(mk_plot, 'Writes', 'write')
 
    -----------------------------------------------------------------------------
    -- main drawing functions
 
-   local update = function()
-      local read_bytes, write_bytes = sys.get_total_disk_io(DEVICE_PATHS)
-      common.update_rate_timeseries(reads, read_bytes)
-      common.update_rate_timeseries(writes, write_bytes)
-   end
+   local rbs = common.reduce_blocks_(
+      'INPUT / OUTPUT',
+      point,
+      geometry.SECTION_WIDTH,
+      {
+         common.mk_block(mk_reads, true, 0),
+         common.mk_block(mk_writes, true, PLOT_SEC_BREAK),
+      }
+   )
 
-   local draw_static = function(cr)
-      common.draw_header(cr, header)
-      common.tagged_scaled_timeseries_draw_static(reads, cr)
-      common.tagged_scaled_timeseries_draw_static(writes, cr)
-   end
-
-   local draw_dynamic = function(cr)
-      common.tagged_scaled_timeseries_draw_dynamic(reads, cr)
-      common.tagged_scaled_timeseries_draw_dynamic(writes, cr)
-   end
-
-   return {static = draw_static, dynamic = draw_dynamic, update = update}
+   return pure.map_at(
+      "update",
+      function(f)
+         return function()
+            state.read, state.write = sys.get_total_disk_io(DEVICE_PATHS)
+            f()
+         end
+      end,
+      rbs)
 end
