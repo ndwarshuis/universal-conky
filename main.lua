c9918ba35871107af4a7537ff9c13a3a1406761f
ndwarshuis <ndwar@yavin4.ch>
2022-07-19 18:11:46 -0400

    REF get root dir from conky.conf

--- main.lua
+++ main.lua
@@ -1,35 +1,44 @@
-local conky_dir = debug.getinfo(1).source:match("@?(.*/)")
-local subdirs = {
-   '?.lua',
-   'drawing/?.lua',
-   'schema/?.lua',
-   'core/?.lua',
-   'core/widget/?.lua',
-   'core/widget/arc/?.lua',
-   'core/widget/text/?.lua',
-   'core/widget/timeseries/?.lua',
-   'core/widget/rect/?.lua',
-   'core/widget/line/?.lua',
-   'lib/share/lua/5.4/?.lua',
-   'lib/share/lua/5.4/?/init.lua',
-}
-
-for i = 1, #subdirs do
-   package.path = package.path..';'..conky_dir..subdirs[i]
-end
-
-package.cpath = package.cpath..';'..conky_dir..'lib/lib/lua/5.4/?.so;'
-
 --------------------------------------------------------------------------------
 -- startup - this is where the modules are compiled and arranged
 
-require 'cairo'
-
-local compile = require 'compile'
-
 local draw_dynamic
 
-function conky_start(update_interval)
+local __cairo_xlib_surface_create
+local __cairo_create
+local __cairo_surface_destroy
+local __cairo_destroy
+
+function conky_start(update_interval, conky_dir)
+   local subdirs = {
+      '?.lua',
+      'drawing/?.lua',
+      'schema/?.lua',
+      'core/?.lua',
+      'core/widget/?.lua',
+      'core/widget/arc/?.lua',
+      'core/widget/text/?.lua',
+      'core/widget/timeseries/?.lua',
+      'core/widget/rect/?.lua',
+      'core/widget/line/?.lua',
+      'lib/share/lua/5.4/?.lua',
+      'lib/share/lua/5.4/?/init.lua',
+   }
+
+   for i = 1, #subdirs do
+      package.path = package.path..';'..conky_dir..subdirs[i]
+   end
+
+   package.cpath = package.cpath..';'..conky_dir..'lib/lib/lua/5.4/?.so;'
+
+   require 'cairo'
+
+   __cairo_xlib_surface_create = cairo_xlib_surface_create
+   __cairo_create = cairo_create
+   __cairo_surface_destroy = cairo_surface_destroy
+   __cairo_destroy = cairo_destroy
+
+   local compile = require 'compile'
+
    conky_set_update_interval(update_interval)
 
    draw_dynamic = compile(update_interval, conky_dir..'config.yml')
@@ -37,12 +46,6 @@ end
 
 --------------------------------------------------------------------------------
 -- main loop - where all the drawing/updating happens
-
-
-local __cairo_xlib_surface_create 	= cairo_xlib_surface_create
-local __cairo_create 				= cairo_create
-local __cairo_surface_destroy 		= cairo_surface_destroy
-local __cairo_destroy 				= cairo_destroy
 
 local updates = -2 -- this accounts for the first few spazzy iterations
 
